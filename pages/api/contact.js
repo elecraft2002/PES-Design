b628fbaabdae26c898770fe9354599de150f5525
Vojtech Suchanek <suchavo3@cvut.cz>
2023-04-23 10:25:25 +0200

    email

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,8 +1,13 @@
 import { render } from "@react-email/render";
 import WelcomeTemplate from "../../emails/WelcomeEmail";
 import { sendEmail } from "../../lib/email";
+import nodemailer from "nodemailer";
 
 export default async function handler(req, res) {
+  if (req.method !== "POST") {
+    res.status(405).json({ error: "Method not allowed" });
+    return;
+  }
   // Get data submitted in request's body.
   const body = req.body;
 
